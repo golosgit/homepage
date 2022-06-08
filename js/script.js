fe6ae23ba9c60608d8bd93d943ee8769fb5994b2
Grzegorz Gołos <golosgit@gmail.com>
2022-06-08 19:24:27 +0200

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-console.log("Witam deva!");
+{
+  const welcomeMessage = () => {
+    console.log("Witam deva!");
+  };
 
-let button = document.querySelector(".body__button");
-let buttonText = document.querySelector(".js-buttonText");
-let table = document.querySelector(".table");
+  const hideTableRows = () => {
+    const buttonText = document.querySelector(".js-buttonText");
+    const table = document.querySelector(".table");
 
-button.addEventListener("click", () => {
-  buttonText.innerText =
-    buttonText.innerText === "tylko top 4" ? "top 8" : "tylko top 4";
-  for (const row of table.querySelectorAll("tr:nth-child(1n + 6)")) {
-    row.classList.toggle("table--top8");
-  }
-});
+    buttonText.innerText =
+      buttonText.innerText === "tylko top 4" ? "top 8" : "tylko top 4";
+    for (const row of table.querySelectorAll("tr:nth-child(1n + 6)")) {
+      row.classList.toggle("table--top8");
+    }
+  };
+
+  const init = () => {
+    const button = document.querySelector(".body__button");
+
+    welcomeMessage();
+
+    button.addEventListener("click", hideTableRows);
+  };
+
+  init();
+}
